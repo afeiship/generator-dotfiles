f96de6939e17a73ecc2fc25360d683fe6a813938
afeiship <1290657123@qq.com>
2022-04-29 13:46:29 +0800

    feat: option to ideable

--- __tests__/prettier.spec.js
+++ __tests__/prettier.spec.js
@@ -5,15 +5,15 @@ const helpers = require('yeoman-test');
 const GEN_PATH = path.join(__dirname, '../generators/prettier');
 
 describe('module:prettier', () => {
-  test('creates prettier files: has_idea-true', async () => {
-    await helpers.run(GEN_PATH).withPrompts({ has_idea: true });
+  test('creates prettier files: ideable-true', async () => {
+    await helpers.run(GEN_PATH).withOptions({ ideable: true });
     // normal files
     assert.file(['.idea/prettier.xml', '.prettierrc', '.prettierignore']);
   });
 
-  test('creates prettier files: has_idea-false', async () => {
+  test('creates prettier files: ideable-false', async () => {
     // no .idea/prettier.xml
-    await helpers.run(GEN_PATH).withPrompts({ has_idea: false });
+    await helpers.run(GEN_PATH).withOptions({ ideable: false });
     assert.file(['.prettierrc', '.prettierignore']);
     assert.noFile(['.idea/prettier.xml']);
   });
--- generators/prettier/index.js
+++ generators/prettier/index.js
@@ -2,21 +2,11 @@
 const Generator = require('yeoman-generator');
 const globby = require('globby');
 
+// yo @jswork/dotfiles:prettier --ideable
+
 module.exports = class extends Generator {
-  prompting() {
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'has_idea',
-        message: 'Idea settigns include?'
-      }
-    ];
-
-    return this.prompt(prompts).then((props) => (this.props = props));
-  }
-
   writing() {
-    const pattern = this.props.has_idea ? '**' : '.pre*';
+    const pattern = this.options.ideable ? '**' : '.pre*';
     const opts = { dot: true };
     this.fs.copyTpl(
       globby.sync(this.templatePath(pattern), opts),
